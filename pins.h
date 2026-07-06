3cf917dd989082c47555825b5df616113d57e7a0
jlmarcosb <jlmarcosb@gmail.com>
2026-07-06 11:45:31 +0200

    pins.h

---
+++ pins.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define TFT_BL_PIN 26
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS   5
+#define TFT_DC   2
+#define TFT_RST  4
+
+#define LED_PIN 27
+#define NUM_LEDS 1	// 241
+
+#define ENCODER_A 32
+#define ENCODER_B 33
+#define ENCODER_SW 25
+
+#define BUTTON2_PIN 34
